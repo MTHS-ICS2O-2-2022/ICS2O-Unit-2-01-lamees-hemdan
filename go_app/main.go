c44cb43af2c2ad8960e2abb10764e6fef71a8b30
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-08 17:29:52 +0000

    update Go program

--- go_app/main.go
+++ go_app/main.go
@@ -1,6 +1,16 @@
-// Created by: Mr Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 //
 // This program displays, "Hello, World!"
 
 package main
+
+import "fmt"
+
+func main() {
+
+	// This function displays, "Hello, World!"
+	// the Go standard is to indent using "tabs" NOT spaces!
+	fmt.Println("Hello, World!")
+	fmt.Println("\nDone.")
+}
